adaa0f6ad22b8ff298bc8b002f26d5cd7faad91d
ievgenii chernenko <chernenko@gmail.com>
2017-10-21 22:02:05 +0200

    heredoc syntax check

---
+++ heredoc-syntax.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+use Modern::Perl '2015';
+use autodie;
+
+my $blurb =<<'END_BLURB';
+    He looked up. "Change is the constant on which they all
+    can agree. We instead, born out of time, remain perfect
+    and perfectly self-aware. We only suffer change as we
+    pursue it. It is against our nature. We rebel against
+    that change. Shall we consider them greater for it?"
+END_BLURB
+
+print $blurb;
