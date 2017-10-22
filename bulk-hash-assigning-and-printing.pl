2618218d99cc6424e3fa6a3594088834b6f848c4
ievgenii chernenko <chernenko@gmail.com>
2017-10-22 15:30:24 +0200

    Data::Printer pretty prints

--- bulk-hash-assigning-and-printing.pl
+++ bulk-hash-assigning-and-printing.pl
@@ -1,10 +1,8 @@
 #!/usr/bin/env perl
 use Modern::Perl '2015';
 use autodie;
-use Data::Dumper;
+use Data::Printer colored => 1;
 
 my %cats = map { $_ => 88 } qw( Jack Brad Mars Grumpy );
 
-while (my ($key, $val) = each %cats) {
-    print "$key => $val \n";
-}
+p(%cats);
