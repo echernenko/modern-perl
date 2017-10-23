ca67aa5dd5ddcc0d4220f5162323db92258af55d
ievgenii chernenko <chernenko@gmail.com>
2017-10-23 21:34:26 +0200

    Perl experimental functions - no arguments case

--- experimental-functions.pl
+++ experimental-functions.pl
@@ -12,5 +12,14 @@ sub greet_another($name = 'Bruce') {
     print "Hello, $name!\n";
 }
 
+# explicitly saying we do not want any arguments
+sub no_gifts_please() {
+    say 'I have too much stuff already.'
+}
+
 greet_one('Robert');
 greet_another();
+# line below will throw an error
+# Too many arguments for subroutine 'main::no_gifts_please'
+# at experimental-functions.pl line 21.
+no_gifts_please('coffee', 'chocolate');
